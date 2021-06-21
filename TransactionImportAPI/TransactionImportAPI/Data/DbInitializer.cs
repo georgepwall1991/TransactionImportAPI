738e61ad38a0638f1c328f1ec6ac907fc5b44ead
George Wall <georgewall1991@icloud.com>
2021-06-21 23:22:54 +0100

    add seed to migrations

---
+++ TransactionImportAPI/TransactionImportAPI/Data/DbInitializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using TransactionImportAPI.Data.DTO;
+using TransactionImportAPI.Model;
+
+namespace TransactionImportAPI.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(TransactionDbContext context)
+        {
+            if (context.Transactions.Any()) return; // db has been seeded
+
+            var countries = new[]
+            {
+                new Country {CountryName = "Great Britain", ISOCode = "GBP", ISOCodeID = 1},
+                new Country {CountryName = "Japan", ISOCode = "JPY", ISOCodeID = 2},
+                new Country {CountryName = "United States Of America", ISOCode = "USD", ISOCodeID = 3}
+            };
+
+            context.Countries.AddRangeAsync(countries);
+            context.SaveChangesAsync();
+
+            var transactionStatus = new[]
+            {
+                new TransactionStatus
+                {
+                    TransactionStatusId = 1,
+                    TransactionStatusType = "Approved"
+                },
+                new TransactionStatus
+                {
+                    TransactionStatusId = 2,
+                    TransactionStatusType = "Rejected"
+                },
+                new TransactionStatus
+                {
+                    TransactionStatusId = 3,
+                    TransactionStatusType = "Done"
+                }
+            };
+
+            context.TransactionStatuses.AddRangeAsync(transactionStatus);
+            context.SaveChangesAsync();
+            
+        }
+    }
+}
--- TransactionImportAPI/TransactionImportAPI/Model/TransactionDbContext.cs
+++ TransactionImportAPI/TransactionImportAPI/Model/TransactionDbContext.cs
@@ -20,50 +20,7 @@ namespace TransactionImportAPI.Model
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Transaction>().ToTable("Transaction");
-            modelBuilder.Entity<Country>().ToTable("Country");
-            modelBuilder.Entity<TransactionStatus>().ToTable("TransactionStatus");
-
-
-            modelBuilder.Entity<Country>()
-                .HasData(
-                    new Country
-                    {
-                        CountryName = "Great Britain",
-                        ISOCode = "GBP",
-                        ISOCodeID = 1
-                    },
-                    new Country
-                    {
-                        CountryName = "Japan",
-                        ISOCode = "JPY",
-                        ISOCodeID = 2
-                    },
-                    new Country
-                    {
-                        CountryName = "United States Of America",
-                        ISOCode = "USD",
-                        ISOCodeID = 3
-                    });
-
-
-            modelBuilder.Entity<TransactionStatus>()
-                .HasData(
-                    new TransactionStatus
-                    {
-                        TransactionStatusId = 1,
-                        TransactionStatusType = "Approved"
-                    },
-                    new TransactionStatus
-                    {
-                        TransactionStatusId = 2,
-                        TransactionStatusType = "Rejected"
-                    },
-                    new TransactionStatus
-                    {
-                        TransactionStatusId = 3,
-                        TransactionStatusType = "Done"
-                    });
+            
         }
 
     }
